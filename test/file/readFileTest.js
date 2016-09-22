72ed1d4c9928dde777bc970d2a176887a450af74
wojcieck <kamil.wojciechowsk@contractors.roche.com>
2016-09-22 10:13:47 +0200

    readFileTests

--- test/file/readFileTest.js
+++ test/file/readFileTest.js
@@ -1,13 +1,25 @@
 const assert = require("assert");
 const coMocha = require("co-mocha");
-const readFile = require("../../lib/file/readFile")
-
+const fs = require("fs");
+const readFile = require("../../lib/file/readFile");
+"use strict";
 describe("Read file", function () {
     it("[integration test] should get file content", function *() {
-        const read = readFile();
+        const read = readFile({fs});
 
         const result = yield read('./symbols.txt');
 
         assert.equal(result, 'GOOG\r\nAAPL\r\nORCL\r\nMSFT\r\nINVALID');
+    });
+
+    
+    it("[integration test] should fail on non existent file read", function *() {
+        const read = readFile({fs});
+        try {
+            yield read("./symbols_invalid.txt");
+            throw "Should never get here";
+        } catch(e) {
+            assert.equal(e,"Cannot read file ./symbols_invalid.txt");
+        }
     });
 });
