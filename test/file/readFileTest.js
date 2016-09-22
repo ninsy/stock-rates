a7c9fbf74f2cb02d2053b59dff855a0434c22514
wojcieck <kamil.wojciechowsk@contractors.roche.com>
2016-09-22 10:44:16 +0200

    readFile unit tests

--- test/file/readFileTest.js
+++ test/file/readFileTest.js
@@ -1,10 +1,14 @@
+"use strict";
+
 const assert = require("assert");
 const coMocha = require("co-mocha");
-const fs = require("fs");
 const readFile = require("../../lib/file/readFile");
-"use strict";
+const td = require("testdouble");
+
 describe("Read file", function () {
     it("[integration test] should get file content", function *() {
+        const fs = require("fs");
+
         const read = readFile({fs});
 
         const result = yield read('./symbols.txt');
@@ -12,8 +16,71 @@ describe("Read file", function () {
         assert.equal(result, 'GOOG\r\nAAPL\r\nORCL\r\nMSFT\r\nINVALID');
     });
 
-    
+    it("[unit test] should get file content", function *() {
+        const fs = {
+            readFile(fileName, encoding, callback) {
+                assert.equal(encoding, "utf-8");
+                assert.equal(fileName, "./symbols.txt");
+                callback(null, "content");
+            }
+        };
+        const read = readFile({fs});
+
+        const result = yield read("./symbols.txt");
+
+        assert.equal(result, "content");
+    });
+
+    it("[unit test] should fail on non-existent file", function *() {
+        const fs = {
+            readFile(fileName, encoding, callback) {
+                assert.equal(encoding, "utf-8");
+                assert.equal(fileName, "./symbols-invalid.txt");
+                callback(`Cannot read file ${fileName}`, null);
+            }
+        };
+        const read = readFile({fs});
+
+        try {
+            yield read("./symbols-invalid.txt");
+            throw "Should never get here";
+        } catch (e) {
+            assert.equal(e, "Cannot read file ./symbols-invalid.txt");
+        }
+    });
+
+
+    it("[unit test with mock lib] should fail on non-existent file", function *() {
+        const fs = td.object();
+
+        td.when(fs.readFile("./symbols-invalid.txt", "utf-8")).thenCallback("Cannot read file ./symbols-invalid.txt",null);
+
+        const read = readFile({fs});
+        try {
+            yield read("./symbols-invalid.txt");
+            throw "Should never get here";
+        } catch(e) {
+            assert.equal(e,"Cannot read file ./symbols-invalid.txt" );
+        }
+
+    });
+
+    it("[unit test with mock lib] should get file content", function *() {
+        const fs = td.object();
+
+        td.when(fs.readFile("./symbols.txt", "utf-8")).thenCallback(null, "content");
+
+        const read = readFile({fs});
+
+        const result = yield read("./symbols.txt");
+
+        assert.equal(result, "content");
+    });
+
+
     it("[integration test] should fail on non existent file read", function *() {
+        const fs = require("fs");
+
         const read = readFile({fs});
         try {
             yield read("./symbols_invalid.txt");
